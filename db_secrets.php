aa46f3a86cd7c1617a12518945ec90ecc5200353
LouieMRD <124847252+LouieMRD@users.noreply.github.com>
2024-12-04 14:08:07 +0000

    Add files via upload

---
+++ db_secrets.php
@@ -0,0 +1,14 @@
+<?php
+// Edel Sherratt October 2023
+
+// place this outside the webserver document tree, and don't keep it in version control on GitLab
+// as it will contain sensitive information once you add your login and password
+
+const DB_DRIVER = 'pgsql';
+const DB_HOST = 'db.dcs.aber.ac.uk';
+const DB_NAME = 'cs25320_24_25_xxxyyy';  // change this so that xxxyyy is your Uni login
+const DB_USER = 'xxx';  // and change this in the same way
+const DB_PASSWORD = 'xxx';  // your PostgreSQL password goes here
+
+
+
